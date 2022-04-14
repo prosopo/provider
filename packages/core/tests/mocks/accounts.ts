3681872cad26ec80a76af4164e0058231af0bb50
seht <seht@hyx.net>
2022-04-14 16:11:14 +0100

    fix contract BigNumber types

--- packages/core/tests/mocks/accounts.ts
+++ packages/core/tests/mocks/accounts.ts
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with provider.  If not, see <http://www.gnu.org/licenses/>.
 import { Payee } from '@prosopo/contract'
+import { BigNumber } from '@prosopo/contract/types';
 
 export interface TestAccount {
     mnemonic: string
@@ -24,7 +25,7 @@ export interface TestProvider extends TestAccount {
     serviceOrigin: string,
     fee: number,
     datasetFile: string,
-    stake: bigint | number,
+    stake: BigNumber,
     payee: Payee
     captchaDatasetId: string,
 }
@@ -45,7 +46,7 @@ export interface TestDapp {
     mnemonic: string,
     contractAccount: string,
     optionalOwner: string,
-    fundAmount: bigint | number,
+    fundAmount: BigNumber,
 }
 
 export const DAPP: TestDapp = {
