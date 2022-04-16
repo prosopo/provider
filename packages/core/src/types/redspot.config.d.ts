0a2d2eb0d02e97923b412ab49d41e9abc13a9094
seht <seht@hyx.net>
2022-04-16 01:22:25 +0100

    BigNumber + BN.js

--- packages/core/src/types/redspot.config.d.ts
+++
@@ -1,8 +0,0 @@
-import '@redspot/patract';
-import '@redspot/chai';
-import '@redspot/watcher';
-import '@redspot/explorer';
-import '@redspot/decimals';
-import { RedspotUserConfig } from 'redspot/types';
-declare const _default: RedspotUserConfig;
-export default _default;
