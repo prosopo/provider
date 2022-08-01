d6843f3272f9176703c712935afc310b436df717
Chris Taylor <forgetso86@gmail.com>
2022-08-01 20:55:57 +0100

    Surface db connect error and comment out test due to compatibility issues with MongoMemoryServer

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,11 +1,11 @@
 // Copyright 2021-2022 Prosopo (UK) Ltd.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -70,7 +70,7 @@ export class ProsopoDatabase implements Database {
             this.tables.pending = db.collection('pending');
 
         } catch (err) {
-            throw new Error(ERRORS.DATABASE.CONNECT_ERROR.message);
+            throw new ProsopoEnvError(err, ERRORS.DATABASE.CONNECT_ERROR.message);
         }
     }
 
